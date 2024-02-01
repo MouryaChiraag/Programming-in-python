90c5e64569d34b97cd2d8298eb6ea2d76b87d143
Mourya <mouryachiraag@gmail.com>
2024-02-01 16:08:44 +0530

    Create swapping_var_using_third_var.py

---
+++ swapping_var_using_third_var.py
@@ -0,0 +1,7 @@
+x = int(input("Enter the value of x before swapping : "))
+y = int(input("Enter the value of y before swapping : "))
+z = x
+x = y
+y = z
+print("The value of x after swapping is : ", x )
+print("The value of x after swapping is : ", y )
